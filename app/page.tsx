c040b0284482190390e5cab50b431f15a965ad03
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-10 11:43:39 +0800

    Separate nav bar into its corresponding component

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,11 @@
 import Image from "next/image";
 
+import { NavBar } from "@/src/components/header/nav-bar";
+
 export default function Home() {
   return (
     <header>
-      <nav className="bg-gray-900">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <a href="#" className="text-white font-semibold text-lg">
-                  Logo
-                </a>
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Features
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Style Guides
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Tags
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Authors
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    More
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center md:ml-6">
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                  Sign In
-                </a>
-                <a
-                  href="#"
-                  className="bg-indigo-500 text-white px-4 py-2 rounded-md text-sm font-medium">
-                  Become a Member
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <NavBar />
     </header>
   );
 }
