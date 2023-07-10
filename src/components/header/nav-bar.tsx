8308ceeaea55eb1b9ef9a829b62a96e95510d512
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-10 12:00:26 +0800

    Fix broken Tailwind classes
    I failed xd

--- src/components/header/nav-bar.tsx
+++ src/components/header/nav-bar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export const NavBar = () => (
-  <nav className="bg-gray-900">
+  <nav className="bg-red-500">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-16">
         <div className="flex items-center">
@@ -14,7 +14,7 @@ export const NavBar = () => (
             <div className="ml-10 flex items-baseline space-x-4">
               <a
                 href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                className="text-slate-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 Features
               </a>
               <a
