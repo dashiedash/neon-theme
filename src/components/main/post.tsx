847baf2850621d148c5c98a384795dc2c66bca34
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-12 16:09:48 +0800

    Create PostImage component

--- src/components/main/post.tsx
+++ src/components/main/post.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const PostHeader = (props: { posts: any }) => {
   const post = props.posts;
 
@@ -22,9 +24,7 @@ export const PostHeader = (props: { posts: any }) => {
               d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
             />
           </svg>
-          <p className="text-xs text-neutral-500">
-            Apurba Talukdar & Ishan Sharma
-          </p>
+          <p className="text-xs text-neutral-500">{post.author}</p>
         </div>
 
         {/* Post Meta: Publish Date */}
@@ -42,7 +42,7 @@ export const PostHeader = (props: { posts: any }) => {
               d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
             />
           </svg>
-          <p className="text-xs text-neutral-500">May 2, 2022</p>
+          <p className="text-xs text-neutral-500">{post.publishDate}</p>
         </div>
 
         {/* Post Meta: Read Duration */}
@@ -60,7 +60,7 @@ export const PostHeader = (props: { posts: any }) => {
               d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
-          <p className="text-xs text-neutral-500">4 min read</p>
+          <p className="text-xs text-neutral-500">{post.readTime}</p>
         </div>
       </div>
     </div>
@@ -71,8 +71,50 @@ export const PostQuoteBlock = (props: { quote: any }) => {
   const quote = props.quote;
 
   return (
-    <div className="px-5 border-l-4 border-rose-500">
-      <blockquote className="text-lg font-medium my-5">{quote}</blockquote>
+    <div className="px-5 border-l-4 border-rose-500 my-16">
+      <blockquote className="text-lg font-medium">{quote}</blockquote>
     </div>
+  );
+};
+
+export const PostImage = (props: {
+  attribName: any;
+  attribNameLink: any;
+  attribSource: any;
+  attribSourceLink: any;
+  imageSource: any;
+}) => {
+  const imageSource = props.imageSource;
+  const attribName = props.attribName;
+  const attribNameLink = props.attribNameLink;
+  const attribSource = props.attribSource;
+  const attribSourceLink = props.attribSourceLink;
+
+  return (
+    <figure className="mt-4 mb-8">
+      <Image
+        src={imageSource}
+        alt=""
+        width={600}
+        height={400}
+        className="rounded-xl"
+      />
+      <figcaption className="text-center text-sm my-2">
+        Photo by{" "}
+        <a
+          href={attribNameLink}
+          target="_blank"
+          className="underline underline-offset-2">
+          {attribName}
+        </a>{" "}
+        /{" "}
+        <a
+          href={attribSourceLink}
+          target="_blank"
+          className="underline underline-offset-2">
+          {attribSource}
+        </a>
+      </figcaption>
+    </figure>
   );
 };
