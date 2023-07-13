41b73f015c0b4e496ae6947559953f472ea11b3a
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-13 14:45:52 +0800

    Clean up code

--- src/components/main/article.tsx
+++ src/components/main/article.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 
 // Article Header
-export const ArticleHeader = (props: { posts: any }) => {
-  const post = props.posts;
+export const ArticleHeader = ({ posts }: { posts: any }) => {
+  const { title, author, publishDate, readTime } = posts;
+
+  const formatAuthor = (authors: string[]) => {
+    if (authors.length > 1) {
+      const lastAuthor = authors[authors.length - 1];
+      const remainingAuthors = authors.slice(0, -1).join(", ");
+      return `${remainingAuthors}, & ${lastAuthor}`;
+    }
+    return authors[0];
+  };
 
   return (
-    <div className="">
+    <div>
       {/* Article Title */}
-      <h1 className="text-4xl font-bold mt-7 mb-5">{post.title}</h1>
+      <h1 className="text-4xl font-bold mt-7 mb-5">{title}</h1>
 
       <div className="flex my-5 space-x-6">
         {/* Article Meta: Author */}
@@ -25,13 +34,7 @@ export const ArticleHeader = (props: { posts: any }) => {
               d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
             />
           </svg>
-          <p className="text-xs text-neutral-500">
-            {post.author.length > 1
-              ? `${post.author.slice(0, -1).join(", ")} & ${post.author.slice(
-                  -1
-                )}`
-              : post.author[0]}
-          </p>
+          <p className="text-xs text-neutral-500">{formatAuthor(author)}</p>
         </div>
 
         {/* Article Meta: Publish Date */}
@@ -49,7 +52,7 @@ export const ArticleHeader = (props: { posts: any }) => {
               d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
             />
           </svg>
-          <p className="text-xs text-neutral-500">{post.publishDate}</p>
+          <p className="text-xs text-neutral-500">{publishDate}</p>
         </div>
 
         {/* Article Meta: Read Duration */}
@@ -67,7 +70,7 @@ export const ArticleHeader = (props: { posts: any }) => {
               d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
-          <p className="text-xs text-neutral-500">{post.readTime}</p>
+          <p className="text-xs text-neutral-500">{readTime}</p>
         </div>
       </div>
     </div>
@@ -75,9 +78,7 @@ export const ArticleHeader = (props: { posts: any }) => {
 };
 
 // Article Quote Block
-export const ArticleQuoteBlock = (props: { quote: any }) => {
-  const quote = props.quote;
-
+export const ArticleQuoteBlock = ({ quote }: { quote: any }) => {
   return (
     <div className="px-5 border-l-4 border-rose-500 my-16">
       <blockquote className="text-lg font-medium">{quote}</blockquote>
@@ -86,19 +87,19 @@ export const ArticleQuoteBlock = (props: { quote: any }) => {
 };
 
 // Article Image
-export const ArticleImage = (props: {
-  attribName: any;
-  attribNameLink: any;
-  attribSource: any;
-  attribSourceLink: any;
-  imageSource: any;
+export const ArticleImage = ({
+  attribName,
+  attribNameLink,
+  attribSource,
+  attribSourceLink,
+  imageSource,
+}: {
+  attribName: string;
+  attribNameLink: string;
+  attribSource: string;
+  attribSourceLink: string;
+  imageSource: string;
 }) => {
-  const imageSource = props.imageSource;
-  const attribName = props.attribName;
-  const attribNameLink = props.attribNameLink;
-  const attribSource = props.attribSource;
-  const attribSourceLink = props.attribSourceLink;
-
   return (
     <figure className="mt-4 mb-8">
       <Image
@@ -113,6 +114,7 @@ export const ArticleImage = (props: {
         <a
           href={attribNameLink}
           target="_blank"
+          rel="noopener noreferrer"
           className="underline underline-offset-2">
           {attribName}
         </a>{" "}
@@ -120,6 +122,7 @@ export const ArticleImage = (props: {
         <a
           href={attribSourceLink}
           target="_blank"
+          rel="noopener noreferrer"
           className="underline underline-offset-2">
           {attribSource}
         </a>
