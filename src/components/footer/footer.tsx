5df31073758337c18b6bad2863f39635c05b5655
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-14 14:21:25 +0800

    Add Follow Us section in Footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,8 +3,12 @@
 | Utilities |
 -------------
 */
-// Imports
+// Libraries
 import React from "react";
+
+// Outside Components
+import { NewsletterFooter } from "./footercomp";
+import { SocialLinks } from "../main/social-links";
 
 /*
 --------------
@@ -14,10 +18,42 @@ import React from "react";
 // Footer
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 p-8">
-      <p className="text-sm">
-        &copy;2023 Neon - All right Reserved. Proudly Published with Ghost
-      </p>
+    <footer className="bg-white p-8 w-full">
+      <div className="container grid lg:grid-cols-2 gap-x-8">
+        <div className="container">
+          <NewsletterFooter />
+          <section className="my-8">
+            <h2 className="font-bold">Follow Us</h2>
+            <div className="flex">
+              <SocialLinks
+                size={20}
+                twitter="https://twitter.com/gbjsolution"
+              />
+              <SocialLinks
+                size={20}
+                facebook="https://www.facebook.com/gbjsolution"
+              />
+              <SocialLinks
+                size={20}
+                linkedin="https://neon.gbjsolution.com/never-let-your-memories-be-greater-than-your-dreams/#"
+              />
+              <SocialLinks
+                size={20}
+                instagram="https://neon.gbjsolution.com/never-let-your-memories-be-greater-than-your-dreams/#"
+              />
+              <SocialLinks
+                size={20}
+                github="https://neon.gbjsolution.com/never-let-your-memories-be-greater-than-your-dreams/#"
+              />
+            </div>
+          </section>
+          <p className="text-sm">
+            &copy;2023 Neon - All right Reserved. Proudly Published with Ghost
+          </p>
+        </div>
+        <div className="container"></div>
+        <div className="container"></div>
+      </div>
     </footer>
   );
 };
